1cb40399245a38696a2adbf4e5b1ae5e35f3995e
Tim Caswell <tim@creationix.com>
2016-03-02 09:59:27 -0800

    Add test using mock kafka

---
+++ src/bfshim/main_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+  "observations"
+  "encoding/json"
+  "log"
+  "testing"
+  "github.com/Shopify/sarama"
+  "bufio"
+  "os"
+  "github.com/Shopify/sarama/mocks"
+)
+
+func TestKafkaRead(t *testing.T) {
+
+  log.Print("Loading test data...")
+
+  file, err := os.Open("../../dump.json")
+  if err != nil { t.Fatal(err) }
+  defer file.Close()
+
+  scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+  consumer := mocks.NewConsumer(t, &sarama.Config{ChannelBufferSize:85000})
+
+  for scanner.Scan() {
+		if err != nil {
+			log.Fatal(err)
+		}
+    json := scanner.Text()
+    expectation := consumer.ExpectConsumePartition("observations.json", 0, sarama.OffsetOldest)
+    expectation.YieldMessage(&sarama.ConsumerMessage{Value: []byte(json)})
+  }
+
+  log.Print("Consuming test data...")
+  reader, err := consumer.ConsumePartition("observations.json", 0, sarama.OffsetOldest)
+  if err != nil { t.Fatal(err) }
+
+  for {
+    message := <-reader.Messages()
+    var obj *observations.Observation
+
+    log.Print(string(message.Value))
+  	err := json.Unmarshal(message.Value, &obj)
+  	if err != nil {
+  		log.Fatal(err)
+  	}
+
+    // log.Print(obj)
+  }
+
+}
